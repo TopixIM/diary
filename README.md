7cf867c3bdb07df972f2b92c7f03d8fb4a629f24
jiyinyiyong <jiyinyiyong@gmail.com>
2018-06-23 01:43:28 +0800

    fix updater; upgrade deps

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Diary
 
 > A tiny app for putting diaries.
 
+Preview http://diary.topix.im
+
 ### Workflow
 
 https://github.com/Cumulo/cumulo-workflow
--- src/app/updater/diary.cljs
+++ src/app/updater/diary.cljs
@@ -9,7 +9,7 @@
   (let [user-id (get-in db [:sessions sid :user-id])]
     (update-in
      db
-     [:users user-id :diaries (:date op-data) (:field op-data)]
+     [:users user-id :diaries (:date op-data)]
      (fn [diary] (-> diary (assoc (:field op-data) (:data op-data)) (assoc :time op-time))))))
 
 (defn set-today [db op-data sid op-id op-time] (assoc db :today op-data))
